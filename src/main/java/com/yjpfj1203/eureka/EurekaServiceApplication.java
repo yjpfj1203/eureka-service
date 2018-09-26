8ec8e0f505a6bbdf969288e93261c8afea2d2c78
yjpfj1203 <yjpfj1203@163.com>
2018-09-26 15:43:25 +0800

    add submodule config eureka-service

--- src/main/java/com/yjpfj1203/eureka/EurekaServiceApplication.java
+++ src/main/java/com/yjpfj1203/eureka/EurekaServiceApplication.java
@@ -2,10 +2,11 @@ package com.yjpfj1203.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
